532e01cc405780ea157ddec2e68cc58085a4d86b
Nour-Zendaki <nour.nour.nr2004@gmail.com>
2025-05-30 00:30:12 +0300

    update images src in cardItems

--- src/components/BestProjects/BestProjects.jsx
+++ src/components/BestProjects/BestProjects.jsx
@@ -5,49 +5,49 @@ import { useEffect, useRef, useState } from 'react'
 
 const cardItems = [
   {
-      image : "/assets/img/EduPress-Project.png",
+      image : "assets/img/EduPress-Project.png",
       projectTitle : "EduPress Project",
       skills : "Html-Css3-ResponsiveDesign",
       url : "https://nour-zendaki.github.io/Mid-Project/"
   },
   {
-      image : "/assets/img/Flora-Project.png",
+      image : "assets/img/Flora-Project.png",
       projectTitle : "Flora Project",
       skills : "Html-Css3-ResponsiveDesign",
       url : "https://nour-zendaki.github.io/Flora-Project/"
   },
   {
-      image : "/assets/img/K2Skiing-Project.png",
+      image : "assets/img/K2Skiing-Project.png",
       projectTitle : "K2Skiing Project",
       skills : "Html-Css3",
       url : "https://nour-zendaki.github.io/K2Skiing-Project/"
   },
   {
-      image : "/assets/img/severalTools-Project.png",
+      image : "assets/img/severalTools-Project.png",
       projectTitle : "severalTools Project",
       skills : "Html-Css3-Vanilla javscript",
       url : "https://nour-zendaki.github.io/Several-Tools-Project/"
   },
   {
-      image : "/assets/img/Evaluation-Project.png",
+      image : "assets/img/Evaluation-Project.png",
       projectTitle : "Evaluation Project",
       skills : "Html-Css3-Vanilla javscript",
       url : "https://nour-zendaki.github.io/Evaluation-Project/"
   },
   {
-      image : "/assets/img/ShoesShoppingProject.png",
+      image : "assets/img/ShoesShoppingProject.png",
       projectTitle : "Shoes Shopping Project",
       skills : "Html-Css3-Vanilla javscript-CRUD",
       url : "https://nour-zendaki.github.io/Shoes-Shopping-Project/"
   },
   {
-      image : "/assets/img/DashBoard-Project.png",
+      image : "assets/img/DashBoard-Project.png",
       projectTitle : "DashBoard Project",
       skills : "Html-Css3-Vanilla javscript-CRUD-API",
       url : "https://nour-zendaki.github.io/DashBoard-Project/"
   },
   {
-      image : "/assets/img/Travel-Project.png",
+      image : "assets/img/Travel-Project.png",
       projectTitle : "Travel Project",
       skills : "Html-Css3",
       url : "https://nour-zendaki.github.io/Travel-Project/"
